42bb4de21e4d12b0776914479239ee89528a2a4c
KasumiNova <3044344887@qq.com>
2022-09-30 00:53:11 +0800

    更新“关于本程序”页面

--- src/main/java/github/kasuminova/balloonserver/GUI/Panels/AboutPanel.java
+++ src/main/java/github/kasuminova/balloonserver/GUI/Panels/AboutPanel.java
@@ -84,6 +84,14 @@ public class AboutPanel {
         openIssuesLink.setPreferredSize(new Dimension(GLOBAL_BUTTON_WIDTH,30));
         linkPanel.add(openIssuesLink);
         descPanel.add(linkPanel);
+
+        descPanel.add(new JLabel("BalloonServer 的诞生离不开这些贡献：", JLabel.CENTER));
+        descPanel.add(new JLabel("Netty 为 BalloonServer 提供了高性能的并发网络框架；", JLabel.CENTER));
+        descPanel.add(new JLabel("Alibaba FastJson2 为 BalloonServer 提供了高性能的 JSON 解析功能；", JLabel.CENTER));
+        descPanel.add(new JLabel("FlatLaf, FlatLaf-Extra 为 BalloonServer 提供了一套完美的用户界面体验；", JLabel.CENTER));
+        descPanel.add(new JLabel("Apache Commons IO 使 BalloonServer 实现了实时文件监听器的功能；", JLabel.CENTER));
+        descPanel.add(new JLabel("以及任何积极使用该软件和为此软件出谋划策的用户和开发者们~", JLabel.CENTER));
+
         //协议
         JLabel licenseLabel = new JLabel("本软件使用 AGPLv3 协议.", JLabel.RIGHT);
         licenseLabel.setFont(licenseLabel.getFont().deriveFont(18f));
