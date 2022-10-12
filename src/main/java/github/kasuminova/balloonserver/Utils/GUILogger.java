5554aa462a10f2674fe5697f1a4461cffadae5a4
KasumiNova <3044344887@qq.com>
2022-10-12 21:22:58 +0800

    将日志文件转移到单独的文件夹中，并且保存先前的日志文件

--- src/main/java/github/kasuminova/balloonserver/Utils/GUILogger.java
+++ src/main/java/github/kasuminova/balloonserver/Utils/GUILogger.java
@@ -21,10 +21,9 @@ import java.util.logging.Logger;
  * 一个简易的 Logger，与 GUI 同步
  */
 public class GUILogger {
-    private final String name;
     private final JTextPane logPane;
     private final Logger logger;
-    private Writer logWriter = null;
+    private final Writer logWriter;
     /**
      * logger 线程池
      * 用于保证 Log 顺序
@@ -32,7 +31,7 @@ public class GUILogger {
     private final ExecutorService loggerThreadPool = Executors.newSingleThreadExecutor();
     private final AtomicInteger prepared = new AtomicInteger(0);
     //日期格式
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private static final Color INFO_COLOR  = new Color(30,170,255);
     private static final Color WARN_COLOR  = new Color(255,215,0);
     private static final Color ERROR_COLOR = new Color(255,75,75);
@@ -48,27 +47,14 @@ public class GUILogger {
 
     /**
      * 创建一个 Logger
-     * @param name 前缀
+     * @param name log 文件名
      * @param logPane 要同步的 JTextPane
      */
     public GUILogger(String name, JTextPane logPane) {
         logger = Logger.getLogger(name);
-        this.name = name;
         this.logPane = logPane;
 
-        File logFile = new File(String.format("./%s.log", name));
-
-        try {
-            if (!logFile.exists()) {
-                if (logFile.createNewFile()) {
-                    this.logWriter = new OutputStreamWriter(Files.newOutputStream(logFile.toPath()), StandardCharsets.UTF_8);
-                }
-            } else {
-                this.logWriter = new OutputStreamWriter(Files.newOutputStream(logFile.toPath()), StandardCharsets.UTF_8);
-            }
-        } catch (Exception e) {
-            logger.warning(stackTraceToString(e));
-        }
+        logWriter = createLogFile(name, logger);
 
         //设置颜色
         StyleConstants.setForeground(INFO_ATTRIBUTE, INFO_COLOR);
@@ -77,26 +63,15 @@ public class GUILogger {
         StyleConstants.setForeground(DEBUG_ATTRIBUTE, DEBUG_COLOR);
     }
 
+    /**
+     * 创建一个 Logger
+     * @param name log 文件名
+     */
     public GUILogger(String name) {
         logger = Logger.getLogger(name);
         logPane = null;
-        this.name = name;
 
-        File logFile = new File(String.format("./%s.log", name));
-
-        try {
-            if (!logFile.exists()) {
-                if (logFile.createNewFile()) {
-                    this.logWriter = new OutputStreamWriter(Files.newOutputStream(logFile.toPath()), StandardCharsets.UTF_8);
-                } else {
-                    logger.warning("创建日志文件失败！");
-                }
-            } else {
-                this.logWriter = new OutputStreamWriter(Files.newOutputStream(logFile.toPath()), StandardCharsets.UTF_8);
-            }
-        } catch (Exception e) {
-            logger.warning(stackTraceToString(e));
-        }
+        logWriter = createLogFile(name, logger);
     }
 
     public void info(String msg) {
@@ -107,7 +82,7 @@ public class GUILogger {
         prepared.getAndIncrement();
         loggerThreadPool.execute(() -> {
             logger.info(msg);
-            String logMessage = buildNormalLogMessage(threadName, msg, INFO, name);
+            String logMessage = buildNormalLogMessage(threadName, msg, INFO);
             writeAndFlushMessage(logMessage);
             insertStringToDocument(logMessage, INFO_ATTRIBUTE);
             prepared.getAndDecrement();
@@ -123,7 +98,7 @@ public class GUILogger {
         prepared.getAndIncrement();
         loggerThreadPool.execute(() -> {
             logger.info(msg);
-            String logMessage = buildNormalLogMessage(threadName, msg, DEBUG, name);
+            String logMessage = buildNormalLogMessage(threadName, msg, DEBUG);
             writeAndFlushMessage(logMessage);
             insertStringToDocument(logMessage, DEBUG_ATTRIBUTE);
             prepared.getAndDecrement();
@@ -138,7 +113,7 @@ public class GUILogger {
         prepared.getAndIncrement();
         loggerThreadPool.execute(() -> {
             logger.warning(msg);
-            String logMessage = buildNormalLogMessage(threadName, msg, WARN, name);
+            String logMessage = buildNormalLogMessage(threadName, msg, WARN);
             writeAndFlushMessage(logMessage);
             insertStringToDocument(logMessage, WARN_ATTRIBUTE);
             prepared.getAndDecrement();
@@ -152,7 +127,7 @@ public class GUILogger {
 
         loggerThreadPool.execute(() -> {
             logger.warning(msg);
-            String logMessage = buildNormalLogMessage(threadName, msg, ERROR, name);
+            String logMessage = buildNormalLogMessage(threadName, msg, ERROR);
             writeAndFlushMessage(logMessage);
             insertStringToDocument(logMessage, ERROR_ATTRIBUTE);
             prepared.getAndDecrement();
@@ -169,7 +144,7 @@ public class GUILogger {
             logger.warning(msg);
             logger.warning(stackTraceToString(e));
             String logMessage = buildNormalLogMessage(threadName,
-                    String.format("%s\n%s", msg, stackTraceToString(e)), ERROR, name);
+                    String.format("%s\n%s", msg, stackTraceToString(e)), ERROR);
             writeAndFlushMessage(logMessage);
             insertStringToDocument(logMessage, ERROR_ATTRIBUTE);
             prepared.getAndDecrement();
@@ -184,7 +159,7 @@ public class GUILogger {
         prepared.getAndIncrement();
         loggerThreadPool.execute(() -> {
             logger.warning(stackTraceToString(e));
-            String logMessage = buildNormalLogMessage(threadName, stackTraceToString(e), ERROR, name);
+            String logMessage = buildNormalLogMessage(threadName, stackTraceToString(e), ERROR);
             writeAndFlushMessage(logMessage);
             insertStringToDocument(logMessage, ERROR_ATTRIBUTE);
             prepared.getAndDecrement();
@@ -243,8 +218,57 @@ public class GUILogger {
         return sw.toString();
     }
 
-    private static String buildNormalLogMessage(String threadName, String msg, String logLevel ,String name) {
+    /**
+     * <p>
+     *     创建一个日志文件。
+     * </p>
+     * <p>
+     *     如果日志文件已存在，则将其有序的重命名（最多保留 50 个）。
+     * </p>
+     * @param name 文件名
+     * @param logger 如果出现问题, 则使用此 logger 警告
+     * @return 一个对应文件名的 Writer, 如果出现问题, 则返回 null
+     */
+    public static Writer createLogFile(String name, Logger logger) {
+        File logFile = new File(String.format("./logs/%s.log", name));
+        try {
+            if (logFile.exists()) {
+                int count = 1;
+                while (count <= 50) {
+                    File oldLogFile = new File(String.format("./logs/%s-%s.log", name, count));
+                    if (oldLogFile.exists()) {
+                        count++;
+                    } else {
+                        if (!logFile.renameTo(oldLogFile)) {
+                            logger.warning("无法正确转移旧日志文件！");
+                        }
+                        break;
+                    }
+                }
+                return new OutputStreamWriter(Files.newOutputStream(new File(String.format("./logs/%s.log", name)).toPath()), StandardCharsets.UTF_8);
+            }
+
+            //检查父文件夹
+            if (!logFile.getParentFile().exists()) {
+                if (!logFile.mkdirs()) {
+                    logger.warning("日志文件夹创建失败！");
+                    return null;
+                }
+            }
+
+            if (logFile.createNewFile()) {
+                return new OutputStreamWriter(Files.newOutputStream(logFile.toPath()), StandardCharsets.UTF_8);
+            } else {
+                logger.warning("创建日志文件失败！");
+            }
+        } catch (Exception e) {
+            logger.warning(stackTraceToString(e));
+        }
+        return null;
+    }
+
+    private static String buildNormalLogMessage(String threadName, String msg, String logLevel) {
         //占位符分别为 日期，线程名，名称，消息本体
-        return String.format("[%s][%s][%s][%s]: %s\n", DATE_FORMAT.format(System.currentTimeMillis()), logLevel, name, threadName, msg);
+        return String.format("[%s][%s][%s]: %s\n", DATE_FORMAT.format(System.currentTimeMillis()), logLevel, threadName, msg);
     }
 }
